520e2fc5d927ab85f2d0ff1f4fb51f7df61c31c7
Saket Bishnu <127406716+Saketbishnu@users.noreply.github.com>
2026-09-22 22:14:31 +0530

    Solved: SQLPBP14 (codechef)

---
+++ codechef/Practice/SQLPBP14.sql
@@ -0,0 +1,9 @@
+// Problem: SQLPBP14
+// Platform: codechef
+// Language: SQL​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/sql-case-studies-topic-wise/SQLBP01/problems/SQLPBP14
+// Solved on: 2026-09-22T16:44:27.113Z
+
+-- your code goes here
+select Avg(salary) As avg_salary from works;
