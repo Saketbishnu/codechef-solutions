47bee07f9f132578997b19781433e466498b96c7
Saket Bishnu <127406716+Saketbishnu@users.noreply.github.com>
2026-09-22 22:14:23 +0530

    Solved: SQLPBP12 (codechef)

---
+++ codechef/Practice/SQLPBP12.sql
@@ -0,0 +1,10 @@
+// Problem: SQLPBP12
+// Platform: codechef
+// Language: SQL​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/sql-case-studies-topic-wise/SQLBP01/problems/SQLPBP12
+// Solved on: 2026-09-22T16:44:19.953Z
+
+-- your code goes here
+select product_name, category from Products
+where price >100;
