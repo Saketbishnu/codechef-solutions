0511770c71379ed30cd2aeae679ee270e7627258
Saket Bishnu <127406716+Saketbishnu@users.noreply.github.com>
2026-09-24 18:34:47 +0530

    Solved: SQLPBP04 (codechef)

--- codechef/Practice/SQLPBP04.c
+++ codechef/Practice/SQLPBP04.c
@@ -9,12 +9,12 @@
 └─────────────┴───────┘
 // Verdict: Accepted
 // URL: https://www.codechef.com/practice/course/sql-case-studies-topic-wise/SQLBP01/problems/SQLPBP04
-// Solved on: 2026-09-24T13:04:07.832Z
+// Solved on: 2026-09-24T13:04:42.557Z
 
 -- your code goes here
 select distinct player_name, score
 from players
-inner join matches
+join matches
 ON players.player_name = matches.winner
 ORDER BY score DESC
 LIMIT 3;
