724f53f962342bf5873e554bb62615ed7d55d182
Saket Bishnu <127406716+Saketbishnu@users.noreply.github.com>
2026-09-26 21:48:21 +0530

    Solved: LPJSPR01 (codechef)

---
+++ codechef/Practice/LPJSPR01.c
@@ -0,0 +1,8 @@
+// Problem: LPJSPR01
+// Platform: codechef
+// Language: console.log(11 + 31);
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/javascript/LPJAS01/problems/LPJSPR01
+// Solved on: 2026-09-26T16:18:18.335Z
+
+// source not captured automatically - copy it from the editor and use Manual Push
