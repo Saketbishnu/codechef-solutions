6e42619d26210a18faf474eddacb5ad67d4e1aa6
Saket Bishnu <127406716+Saketbishnu@users.noreply.github.com>
2026-09-26 21:58:17 +0530

    Solved: LPJSPR10 (codechef)

---
+++ codechef/Practice/LPJSPR10.js
@@ -0,0 +1,9 @@
+// Problem: LPJSPR10
+// Platform: codechef
+// Language: JavaScript​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/javascript/LPJAS01/problems/LPJSPR10
+// Solved on: 2026-09-26T16:28:14.737Z
+
+// your code goes here
+console.log("Learn Coding on CodeChef")
