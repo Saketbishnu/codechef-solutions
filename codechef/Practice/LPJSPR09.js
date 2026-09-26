6104c556f533347264056cda0f0d98cf1f322644
Saket Bishnu <127406716+Saketbishnu@users.noreply.github.com>
2026-09-26 21:57:35 +0530

    Solved: LPJSPR09 (codechef)

---
+++ codechef/Practice/LPJSPR09.js
@@ -0,0 +1,9 @@
+// Problem: LPJSPR09
+// Platform: codechef
+// Language: JavaScript​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/javascript/LPJAS01/problems/LPJSPR09
+// Solved on: 2026-09-26T16:27:32.738Z
+
+// your code goes here
+console.log(12*9)
